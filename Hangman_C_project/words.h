1ae83ac3aa39a4ed6631625a1247b79b7ee35b72
klaudiaaaj <56549544+klaudiaaaj@users.noreply.github.com>
2020-04-06 13:43:41 +0200

    Add files via upload

---
+++ Hangman_C_project/words.h
@@ -0,0 +1,16 @@
+#ifndef Words_HEADER
+#define Words_HEADER
+
+struct Words
+{
+	char* name;
+	char *table[11]; // table with words in the category 
+
+};
+
+
+
+void check(char *table,int  won, int lost);
+void checkcat(int i, int won , int lost);
+
+#endif
